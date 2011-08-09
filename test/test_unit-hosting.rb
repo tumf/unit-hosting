e60f5c36e872fc0f2cb1ca2b23279b7799c50747
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2011-08-09 13:10:17 +0900

    .

--- test/test_unit-hosting.rb
+++ test/test_unit-hosting.rb
@@ -4,10 +4,9 @@ require 'helper'
 class TestUnitHosting < Test::Unit::TestCase
   context "Agent" do
     should "access" do
-      UnitHosting::Agent.new {|agent|
-        agent.getr("/")
-        assert(agent.page.body =~ /ユニットホスティング/,"to top page")
-      }
+      agent = UnitHosting::Agent.new
+      agent.getr("/")
+      assert(agent.page.body =~ /ユニットホスティング/,"to top page")
     end
   end
 end
