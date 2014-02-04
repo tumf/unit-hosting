226fa53861eae9a7725b6d73db8d79361fce98a5
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2014-02-04 14:18:29 +0900

    add UnitHosting::Api::VmRecipe#load_ssh_key specs

--- spec/unit-hosting/api/vm_recipe_spec.rb
+++ spec/unit-hosting/api/vm_recipe_spec.rb
@@ -4,5 +4,24 @@ require 'unit-hosting/api/vm_recipe'
 
 describe UnitHosting::Api::VmRecipe do
   before {
+    @recipe = UnitHosting::Api::VmRecipe.new
   }
+  describe "#load_ssh_key" do
+    before {
+      @ssh_key_file = Tempfile.new('ssh_key')
+      @ssh_key_file.puts(<<"KEY")
+dummy
+ssh-key
+file
+KEY
+      @ssh_key_file.close
+    }
+    after {
+      @ssh_key_file.unlink
+    }
+    it "loads ssh key file " do
+      @recipe.load_ssh_key(@ssh_key_file.path)
+      expect(@recipe.ssh_key).to match /ssh-key/
+    end
+  end
 end
