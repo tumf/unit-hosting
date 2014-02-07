12868227d55d0fc6dfa23d69f3b34dceed95096b
Yoshihiro TAKAHARA <y.takahara@gmail.com>
2014-02-07 14:34:20 +0900

    add silence_output/enable_output spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,3 +29,22 @@ def capture(stream)
   end
   result
 end
+
+# https://gist.github.com/adamstegman/926858
+# Redirects stderr and stdout to /dev/null.
+def silence_output
+  @orig_stderr = $stderr
+  @orig_stdout = $stdout
+ 
+  # redirect stderr and stdout to /dev/null
+  $stderr = File.new('/dev/null', 'w')
+  $stdout = File.new('/dev/null', 'w')
+end
+ 
+# Replace stdout and stderr so anything else is output correctly.
+def enable_output
+  $stderr = @orig_stderr
+  $stdout = @orig_stdout
+  @orig_stderr = nil
+  @orig_stdout = nil
+end
